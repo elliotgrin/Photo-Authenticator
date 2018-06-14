f4d30b8ae29ae593fd2b17b96d10865a5560ce68
rpkuznetsov <rkz@grapheme.ru>
2018-06-14 16:48:54 +0300

    Set on click listener

--- app/src/main/java/com/apps/elliotgrin/authenticator/ui/MainActivity.kt
+++ app/src/main/java/com/apps/elliotgrin/authenticator/ui/MainActivity.kt
@@ -35,7 +35,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        takePictureButton
+        takePictureButton.setOnClickListener { dispatchTakePictureIntent() }
     }
 
     fun dispatchTakePictureIntent() {
